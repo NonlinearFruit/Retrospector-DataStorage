ae64b0db4a73b2496bd7183a40627396551320e1
NonlinearFruit <1123benji5813@gmail.com>
2020-11-06 09:30:05 -0600

    Remove java

--- src/main/java/retrospector/hsqldb/exceptions/ForeignEntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-
-package retrospector.hsqldb.exceptions;
-
-public class ForeignEntityNotFoundException extends RuntimeException{
-
-    public ForeignEntityNotFoundException() {
-        super("Foreign Entity Not Found");
-    }
-}
